83233532df1469c56c351f3f50ffd5cd78077d48
Victor Bispo <VictorBispo05@outlook.com>
2024-08-13 17:39:09 -0300

    commit inical

---
+++ functions.ts
@@ -0,0 +1,13 @@
+function multiplicar(a: number, b: number): number {
+  return a * b;
+}
+
+function ola(nome: string): string {
+  return "Olá " + nome;
+}
+
+const resultado = multiplicar(5, 3); 
+const saudacao = ola("João"); 
+
+console.log(resultado)
+console.log(saudacao)
